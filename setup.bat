92cb04517e06345034704fd8dd79042aed3d988a
nevi-dev <nevijose4@gmail.com>
2025-06-02 02:02:34 -0400

    Actualizar setup.bat

--- setup.bat
+++ setup.bat
@@ -2,39 +2,39 @@
 REM Verificar si Node.js está instalado
 where node >nul 2>nul
 if %errorlevel% neq 0 (
-    echo [Console @ Ruby-Bot] Node.js no esta instalado. Ejecutando instalador...
+    echo [Console @ Vermeil-Bot] Node.js no esta instalado. Ejecutando instalador...
     start /wait node-installer.msi
 ) else (
-    echo [Console @ Ruby-Bot] Node.js ya esta instalado.
+    echo [Console @ Vermeil-Bot] Node.js ya esta instalado.
 )
 
 REM Verificar si Git está instalado
 where git >nul 2>nul
 if %errorlevel% neq 0 (
-    echo [Console @ Ruby-Bot] Git no esta instalado. Ejecutando instalador...
+    echo [Console @ Vermeil-Bot] Git no esta instalado. Ejecutando instalador...
     start /wait git-installer.exe
 ) else (
-    echo [Console @ Ruby-Bot] Git ya esta instalado.
+    echo [Console @ Vermeil-Bot] Git ya esta instalado.
 )
 
 REM Verificar si ImageMagick está instalado
 where convert >nul 2>nul
 if %errorlevel% neq 0 (
-    echo [Console @ Ruby-Bot] ImageMagick no esta instalado. Ejecutando instalador...
+    echo [Console @ Vermeil-Bot] ImageMagick no esta instalado. Ejecutando instalador...
     start /wait imagemagick-installer.exe
 ) else (
     echo [Console @ Ruby-Bot] ImageMagick ya esta instalado.
 )
 
 REM Ejecutar Git pull
-echo [Console @ Ruby-Bot] Verificando Actualizaciones...
+echo [Console @ Vermeil-Bot] Verificando Actualizaciones...
 git pull
 
 REM Ejecutar npm install (ignorar crasheo)
-echo [Console @ Ruby-Bot] Instalando Dependencias...
+echo [Console @ Vermeil-Bot] Instalando Dependencias...
 npm install
 if %errorlevel% neq 0 (
-    echo [Console @ Ruby-Bot] Se detectó un problema después de npm install, pero se ignorará para continuar.
+    echo [Console @ Vermeil-Bot] Se detectó un problema después de npm install, pero se ignorará para continuar.
 )
 
 REM Ejecutar node index.js
