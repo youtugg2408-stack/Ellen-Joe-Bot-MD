d52302c5345e3c0a17f0c412731386ca58d3975f
nevi-dev <nevijose4@gmail.com>
2025-06-02 02:03:54 -0400

    Actualizar run.bat

--- run.bat
+++ run.bat
@@ -1,17 +1,17 @@
 @echo off
-echo [ INFORMACION ] Ruby-Hoshino-Bot-MD para Windows
+echo [ INFORMACION ] Vermeil-Bot para Windows
 
 REM Ejecutar el archivo setup.bat
-echo [Console @ Ruby_Hoshino] Verificando si los programas de ejecucion estan instalados...
+echo [Console @ Vermeil-Bot] Verificando si los programas de ejecucion estan instalados...
 call setup.bat
 
 REM Verificar si setup.bat terminó correctamente
 if %errorlevel% neq 0 (
-    echo [Console @ Ruby_Hoshino] setup.bat termino con errores, pero se continuara con la ejecución.
+    echo [Console @ Vermeil-Bot] setup.bat termino con errores, pero se continuara con la ejecución.
 )
 
 REM Ejecutar node index.js
-echo [Console @ Ruby_Hoshino] Iniciando Ruby-Hoshino-Bot-MD...
+echo [Console @ Vermeil-Bot] Iniciando Vermeil-Bot...
 node index.js
 if %errorlevel% neq 0 (
     echo [ERROR] Ocurrio un error. Verifica la instalacion.
