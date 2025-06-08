29a4c0545c21e932c15a9e927a6e2e67d3a45ff8
Nevi <nevijose4@gmail.com>
2025-06-07 22:31:29 -0400

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -59,7 +59,7 @@ global.avatar = 'https://qu.ax/RYjEw.jpeg'
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 global.gp1 = 'https://chat.whatsapp.com/J5hNkSzRKPSADI72wQk1B3'
-global.comunidad1 = 'https://chat.whatsapp.com/K2CPrOTksiA36SW6k41yuR'
+global.comunidad1 = 'https://chat.whatsapp.com/Ck1VBghTuGx6AcYOKNRtHT'
 global.channel = 'https://whatsapp.com/channel/0029VbAuMiNCBtxOKcBfw71x'
 global.channel2 = 'https://whatsapp.com/channel/0029VbAuMiNCBtxOKcBfw71x'
 global.md = 'https://github.com/nevi-dev/Vermeil-bot'
