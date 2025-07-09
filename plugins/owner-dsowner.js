ffd5e8403becb0e88d7097c155effce4c874505d
Nevi <nevijose4@gmail.com>
2025-07-08 20:10:08 -0400

    Actualizar owner-dsowner.js

--- plugins/owner-dsowner.js
+++ plugins/owner-dsowner.js
@@ -11,7 +11,7 @@ return conn.reply(m.chat, `${emoji} Utiliza este comando directamente en el núm
 await conn.reply(m.chat, `${emoji2} Iniciando proceso de eliminación de todos los archivos de sesión, excepto el archivo creds.json...`, m)
 m.react(rwait)
 
-let sessionPath = `./${Rubysessions}/`
+let sessionPath = `./${Ellensessions}/`
 
 try {
 
