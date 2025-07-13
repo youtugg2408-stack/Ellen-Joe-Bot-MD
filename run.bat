e2858d9a50629062649b55f5d95f75b581c5ab1f
Nevi <nevijose4@gmail.com>
2025-07-13 01:52:20 -0400

    Actualizar run.bat

--- run.bat
+++ run.bat
@@ -1,17 +1,17 @@
 @echo off
-echo [ INFORMACION ] Vermeil-Bot para Windows
+echo [ INFORMACION ] Ellen-Joe-Bot para Windows
 
 REM Ejecutar el archivo setup.bat
-echo [Console @ Vermeil-Bot] Verificando si los programas de ejecucion estan instalados...
+echo [Console @ Ellen-Joe-Bot] Verificando si los programas de ejecucion estan instalados...
 call setup.bat
 
 REM Verificar si setup.bat terminó correctamente
 if %errorlevel% neq 0 (
-    echo [Console @ Vermeil-Bot] setup.bat termino con errores, pero se continuara con la ejecución.
+    echo [Console @ Ellen-Joe-Bot] setup.bat termino con errores, pero se continuara con la ejecución.
 )
 
 REM Ejecutar node index.js
-echo [Console @ Vermeil-Bot] Iniciando Vermeil-Bot...
+echo [Console @ Ellen-Joe-Bot] Iniciando Ellen-Joe-Bot...
 node index.js
 if %errorlevel% neq 0 (
     echo [ERROR] Ocurrio un error. Verifica la instalacion.
