7cae9da8c041c3c2575e3d74830141574eb42113
Nevi <nevijose4@gmail.com>
2025-07-13 01:27:32 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-Process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
 import './settings.js'
 import { setupMaster, fork } from 'cluster'
 import { watchFile, unwatchFile } from 'fs'
