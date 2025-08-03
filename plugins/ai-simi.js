e67ef53de27865a3e7664bcd3dca76c96962692f
Nevi <nevijose4@gmail.com>
2025-08-03 15:11:50 -0400

    Actualizar ai-simi.js

--- plugins/ai-simi.js
+++ plugins/ai-simi.js
@@ -17,7 +17,7 @@ handler.help = ['simi', 'bot'];
 handler.tags = ['fun'];
 handler.group = true;
 handler.register = true
-handler.command = ['Ellen', 'ellen', 'bot', 'simi']
+handler.command = ['Ellen', 'ellen', 'simi']
 
 export default handler;
 
